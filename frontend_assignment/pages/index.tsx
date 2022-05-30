297871b4d66f395bd9633aa45ee98129b9264cef
MarijaMikic <marija.d.mikic@gmail.com>
2022-05-30 22:52:04 +0200

    combine 2 in 1
    week4

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,14 +1,50 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { providers, Contract , utils} from "ethers"
 import Head from "next/head"
+import 'react-toastify/dist/ReactToastify.css'
 import React from "react"
 import styles from "../styles/Home.module.css"
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from "yup"
+
+
+const sema = yup.object({
+    ime: yup.string().required(),
+    godine: yup.number().positive().integer().required(),
+    adresa: yup.string().required(),
+  }).required()
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
-
+    const [greetMessage, setGreetMessage] = React.useState('');
+	React.useEffect(() => {
+		listenGreeting();
+        return () => greeterContract.removeAllListeners();
+	}, []);
+	const listenGreeting = () => {
+		try {
+			const contract = new Contract(
+                '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+				Greeter.abi
+			);
+			const provider = new providers.JsonRpcProvider('http://localhost:8545');
+			const greeterContract = contract.connect(provider.getSigner());
+			greeterContract.on('NewGreeting', (greating: string) => {
+				console.log(utils.parseBytes32String(greating));
+				setGreetMessage(utils.parseBytes32String(greating));
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
+    const { register, handleSubmit } = useForm({
+        resolver: yupResolver(sema)
+      });
+  
     async function greet() {
         setLogs("Creating your Semaphore identity...")
 
@@ -58,7 +94,12 @@ export default function Home() {
             setLogs("Your anonymous greeting is onchain :)")
         }
     }
+    function onSubmit(data: Record <string, any>) {
+        console.log(JSON.stringify(data));
+    }
 
+    
+    
     return (
         <div className={styles.container}>
             <Head>
@@ -77,6 +118,17 @@ export default function Home() {
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
                 </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                <p>Name</p>
+                <input style={{backgroundColor :"grey"}} {...register("ime", { required: true})} />
+                <p>Age</p>
+                <input style={{backgroundColor :"grey"}} {...register("godine", { required: true})} />
+                <p>Address</p>
+                <input style={{backgroundColor :"grey"}} {...register("adresa", { required: true})} />
+                <p></p>
+                <input style={{backgroundColor :"green"}} type="submit"/>
+                <p>New greating: {greetMessage}</p>
+                </form>
             </main>
         </div>
     )
